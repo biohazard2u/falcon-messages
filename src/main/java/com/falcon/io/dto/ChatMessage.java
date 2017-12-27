97040886ce1821e5e0025479b76334a9b300557c
Marcos Zalacain <marcoszalacain@gmail.com>
2017-12-27 22:34:52 +0100

    clean up

--- src/main/java/com/falcon/io/dto/ChatMessage.java
+++ src/main/java/com/falcon/io/dto/ChatMessage.java
@@ -1,8 +1,5 @@
 package com.falcon.io.dto;
 
-/**
- * Created by rajeevkumarsingh on 24/07/17.
- */
 public class ChatMessage {
     private MessageType type;
     private String content;
--- src/main/java/com/falcon/io/repository/postgre/FalconMessagePostgreRepository.java
+++ src/main/java/com/falcon/io/repository/postgre/FalconMessagePostgreRepository.java
@@ -8,10 +8,6 @@ import com.falcon.io.entity.FalconMessageEntity;
 
 public interface FalconMessagePostgreRepository extends CrudRepository<FalconMessageEntity, String> {
 	
-			//extends ReactiveCrudRepository<FalconMessageEntity, String> {
-//	Mono<FalconMessageEntity> findByMessageId(Integer messageId);
-//	Flux<FalconMessageEntity> findByOwner(final String lastname);
-	
 	List<FalconMessageEntity> findByOwner(String owner);
 	
 	void saveFalconMessage(FalconMessageEntity falconMessage);
