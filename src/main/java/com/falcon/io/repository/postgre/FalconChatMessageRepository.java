37ea118ae346327cd971036f48f2761869f543e1
Marcos Zalacain <marcoszalacain@gmail.com>
2018-02-01 11:16:58 +0100

    repo anotation

--- src/main/java/com/falcon/io/repository/postgre/FalconChatMessageRepository.java
+++ src/main/java/com/falcon/io/repository/postgre/FalconChatMessageRepository.java
@@ -3,9 +3,11 @@ package com.falcon.io.repository.postgre;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.falcon.io.entity.FalconChatMessageEntity;
 
+@Repository
 public interface FalconChatMessageRepository extends CrudRepository<FalconChatMessageEntity, Integer> {
 
 	List<FalconChatMessageEntity> findByUserId(Integer userId);
